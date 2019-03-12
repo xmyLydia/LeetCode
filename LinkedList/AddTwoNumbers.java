09f8e6c405da1b23bc28c4971dc13cf23c522f6c
xmyLydia <836728902@qq.com>
2019-03-12 18:58:11 +0200

    Update AddTwoNumbers.java

--- LinkedList/AddTwoNumbers.java
+++ LinkedList/AddTwoNumbers.java
@@ -24,6 +24,13 @@ public class AddTwoNumbers {
             System.out.println(result);
 	}
 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	/* You are given two non-empty linked lists representing two non-negative integers. 
+	 * The digits are stored in reverse order and each of their nodes contain a single digit. 
+	 * Add the two numbers and return it as a linked list.
+	 * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+	 * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+	 * Output: 7 -> 0 -> 8
+	 */
 	if(l1==null&&l2==null) return null;
 	if(l1==null&&l2!=null) {
 		l1 = new ListNode(0);
