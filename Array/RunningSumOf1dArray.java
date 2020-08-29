288e91e83b1680269d52b53e4223f0f3cafa15a0
mxie <836728902@qq.com>
2020-08-30 00:12:00 +0300

    Start leetcode practice: array

---
+++ Array/RunningSumOf1dArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class RunningSumOf1dArray {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public int[] runningSum(int[] nums) {
+//
+//        if (nums != null && nums.length > 1) {
+//            for (int i = 1; i < nums.length; i++) {
+//                nums[i] = nums[0] +
+//            }
+//        }
+//        return nums;
+//    }
+}
