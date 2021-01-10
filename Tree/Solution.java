d84630940934dabf303565a2857abdc29de6722b
mxie <836728902@qq.com>
2021-01-10 23:13:04 +0200

    mock challenge

--- Tree/Solution.java
+++ Tree/Solution.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,4 +52,72 @@ public class Solution {
         }
         return false;
     }
+
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3) {
+            return false;
+        }
+        if (arr.length == 3) {
+            if (arr[1] > arr[0] && arr[1] < arr[2]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        //find the max index
+        int indexMax = 0;
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int val = arr[i];
+            if (maxVal < val) {
+                maxVal = val;
+                indexMax = i;
+            }
+        }
+        if (indexMax == 0 || indexMax == arr.length - 1) {
+            return false;
+        }
+        //partition them into two array
+        //left array will increasing strictly
+        boolean isMountain = true;
+        for (int i = 0; i < indexMax - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        //right array will decreasing strictly
+        for (int i = indexMax; i < arr.length - 1; i++) {
+            if (arr[i] < arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] replaceElements(int[] arr) {
+        //loop each element in the array
+        if (arr.length == 1) {
+            return arr;
+        }
+        for (int i = 0; i < arr.length; i++) {
+
+            int maxForCurrentVal = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = arr[j];
+                if (temp > maxForCurrentVal) {
+                    maxForCurrentVal = temp;
+                }
+            }
+            arr[i] = maxForCurrentVal;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] array = new int[]{0, 3, 2, 1};
+        solution.validMountainArray(array);
+    }
+
 }
